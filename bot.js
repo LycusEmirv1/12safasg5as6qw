2a792329eea97d19818c2641e226f61f760d394d
LycusEmirv1 <42849381+LycusEmirv1@users.noreply.github.com>
2018-11-20 19:39:08 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,431 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const Jimp = require('jimp');
+const db = require('quick.db');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+ 
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+   let props = require(`./komutlar/${f}`);
+log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+      props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+
+
+
+client.on("message", message => {
+
+    const kufur = ["amk","piç","yarrak","ananı sikim","orospu","orospu çoçuğu","siktir git","siktir","anneni sikerim","anneni sikim","amına koduğum","amına koyduğum","gavat","sikik","sikem","kahpe"]
+    if (kufur.some(word =>  message.content.toLowerCase().includes(word)) ) {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) return;
+        message.reply("**Küfür Etme!** :rage:")
+        message.delete()
+    }
+});
+
+
+
+ client.on("message", message => {
+    const reklam = ["discord.gg/"];
+    if (reklam.some(word => message.content.toLowerCase().includes(word)) ) {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) return;  
+        message.reply("**Reklam Yapma!** :rage:")
+        message.delete()
+    }
+   
+
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on("message", async message => {
+  var user = message.mentions.users.first() || message.author;
+    if (message.content.toLowerCase() === prefix + "atatürkefekt") {		
+        var user = message.mentions.users.first() || message.author;
+        if (!message.guild) user = message.author;
+
+        message.channel.send("`Profil Fotoğrafınıza` **Göre Ayarlıyorum. Bu Biraz Zaman Alabilir**").then(m => m.delete(1000));
+
+        Jimp.read(user.avatarURL, (err, image) => {
+            image.resize(400, 400)	
+            image.greyscale()
+            image.gaussian(1)
+            Jimp.read("https://cdn.discordapp.com/attachments/484692865985806346/487849021658890240/image0.png", (err, avatar) => {
+                avatar.resize(400, 400)
+                image.composite(avatar, 0, 0).write(`./img/snip/${client.user.id}-${user.id}.png`);
+                setTimeout(function() {
+                    message.channel.send(new Discord.Attachment(`./img/snip/${client.user.id}-${user.id}.png`));
+                }, 1000);
+            });
+
+        });
+    }
+});
+
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'yeni-gelenler'); 
+  if (!channel) return;
+  member.sendMessage('Hozikman sunucusuna hoşgeldin kardeşim.Sunucumuzda #bilgi-kurallar kısmını okumayı unutmayınız. Youtube Kanalımıza hâlâ abone değilsen https://www.youtube.com/hozikman ne duruyorsun')
+  const embed = new Discord.RichEmbed()
+  .setColor('0x00cc44')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle(`:inbox_tray: ${member.user.username} Sunucuya katıldı.`)
+  .setTimestamp()
+  channel.sendEmbed(embed);
+});
+
+const { Client } = require('discord.js');
+const YouTube = require('simple-youtube-api');
+const yt = require('ytdl-core');
+
+const youtube = new YouTube(ayarlar.api);
+
+
+let queue = {};
+
+const commands = {
+	'play': (msg) => {
+		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`${ayarlar.prefix}ekle <url> veya ismi ile birkaç müzik ekle`);
+		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
+		if (queue[msg.guild.id].playing) return msg.channel.sendMessage('Zaten Çalınan var');
+		let dispatcher;
+		queue[msg.guild.id].playing = true;
+
+		console.log(queue);
+		(function play(song) {
+			console.log(song);
+			if (song === undefined) return msg.channel.sendMessage('Sıra boş').then(() => {
+				queue[msg.guild.id].playing = false;
+				msg.member.voiceChannel.leave();
+			});
+			msg.channel.sendMessage(`Çalınan: **${song.title}** talep eden: **${song.requester}**`);
+			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : ayarlar.passes });
+			let collector = msg.channel.createCollector(m => m);
+			collector.on('message', m => {
+				if (m.content.startsWith(ayarlar.prefix + 'durdur')) {
+					msg.channel.sendMessage('**durduruldu**').then(() => {dispatcher.pause();});
+				} else if (m.content.startsWith(ayarlar.prefix + 'devamet')){
+					msg.channel.sendMessage('**devam ediyor**').then(() => {dispatcher.resume();});
+				} else if (m.content.startsWith(ayarlar.prefix + 'geç')){
+					msg.channel.sendMessage('**geçildi**').then(() => {dispatcher.end();});
+				} else if (m.content.startsWith('volume+')){
+					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Şiddet: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+					msg.channel.sendMessage(`Şiddet: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith('volume-')){
+					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Şiddet: ${Math.round(dispatcher.volume*50)}%`);
+					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+					msg.channel.sendMessage(`Şiddet: ${Math.round(dispatcher.volume*50)}%`);
+				} else if (m.content.startsWith(ayarlar.prefix + 'süre')){
+					msg.channel.sendMessage(`Süre: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+				}
+			});
+			dispatcher.on('end', () => {
+				collector.stop();
+				play(queue[msg.guild.id].songs.shift());
+			});
+			dispatcher.on('error', (err) => {
+				return msg.channel.sendMessage('Hata: ' + err).then(() => {
+					collector.stop();
+					play(queue[msg.guild.id].songs.shift());
+				});
+			});
+		})(queue[msg.guild.id].songs.shift());
+	},
+	'join': (msg) => {
+		return new Promise((resolve, reject) => {
+			const voiceChannel = msg.member.voiceChannel;
+			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('Bir kanala katıl.');
+			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+		});
+	},
+	'ayrıl': (msg) => {
+					const voiceChannel = msg.member.voiceChannel;
+
+			voiceChannel.leave()
+		
+	},
+	'ekle': async (msg) => {
+		const args = msg.content.split(' ');
+		const searchString = args.slice(1).join(' ');
+		const url2 = args[1].replace(/<.+>/g, '1');
+		
+		try {
+			var video = await youtube.getVideo(url2)
+		} catch (error) {
+			try {
+				var videos = await youtube.searchVideos(searchString, 1)
+				var video = await youtube.getVideoByID(videos[0].id)
+			} catch (err) {
+				console.log(err)
+				msg.channel.send('Bir hata oluştu: ' + err)
+			};
+		};
+		
+		var url = `https://www.youtube.com/watch?v=${video.id}`
+		
+		if (url == '' || url === undefined) return msg.channel.sendMessage(`Bir YouTube linki eklemek için ${ayarlar.prefix}ekle <url> yazınız`);
+		yt.getInfo(url, (err, info) => {
+			if(err) return msg.channel.sendMessage('Geçersiz YouTube Bağlantısı: ' + err);
+			if (!queue.hasOwnProperty(msg.guild.id)) queue[msg.guild.id] = {}, queue[msg.guild.id].playing = false, queue[msg.guild.id].songs = [];
+			queue[msg.guild.id].songs.push({url: url, title: info.title, requester: msg.author.username});
+			msg.channel.sendMessage(`sıraya **${info.title}** eklendi`);
+		});
+	},
+	'queue': (msg) => {
+		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Sıraya ilk önce bazı şarkıları ekle : ${ayarlar.prefix}ekle`);
+		let tosend = [];
+		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Talep eden: ${song.requester}`);});
+		msg.channel.sendMessage(`__**${msg.guild.name}'s Müzik Kuyruğu:**__ Şu anda **${tosend.length}** şarkı sırada ${(tosend.length > 15 ? '*[Sadece 15 tanesi gösteriliyor]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+	}
+};
+
+client.on('ready', () => {
+	console.log('ready!');
+});
+
+client.on('message', msg => {
+	if (!msg.content.startsWith(ayarlar.prefix)) return;
+	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(ayarlar.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(ayarlar.prefix.length).split(' ')[0]](msg);
+});
+
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'instagram') {
+    msg.channel.send('https://www.instagram.com/hozikman/?hl=tr');
+    
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'twitch') {
+    msg.channel.send('https://www.twitch.tv/hozikman');
+    
+  }
+});
+
+  client.on('message', message => {
+        if (message.content.toLowerCase() === prefix + "sunucubilgi") {
+        const embed = new Discord.RichEmbed()
+            .addField("Sunucu Adı", message.guild.name, true)
+
+            .addField("Sunucu ID", message.guild.id, true)
+
+            .addField("Sunucu Sahibi", message.guild.owner, true)
+
+            .addField("Toplam Üye Sayısı", message.guild.memberCount, true)
+
+            .addField("AFK Süresi", message.guild.afkTimeout, true)
+
+            .setFooter("Oluşturulma Tarihi " + message.guild.createdAt)
+        
+            .setColor("RANDOM")
+
+        return message.channel.sendEmbed(embed)
+    }
+    });
+
+
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'hozikman') {  
+    msg.channel.send('https://www.youtube.com/channel/UC-PjyQhjKeqQ5YwKkFqCGZA/about');
+  }
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'aga bee') {
+     msg.channel.send('**Aga bee yak yak yak.**')
+     msg.react("🚬");
+  }
+})
+
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'aga b') {
+     msg.channel.send('**Aga bee yak yak yak.**')
+     msg.react("🚬");
+  }
+})
+
+
+
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleyküm selam,  hoş geldin ');
+  }
+});
+  
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'selamun aleyküm') {
+    msg.reply('Aleyküm selam,  hoş geldin');
+  }
+});
+
+const GIFEncoder = require('gifencoder');
+
+client.on("message", async message => {
+  var user = message.mentions.users.first() || message.author;
+    if (message.content.toLowerCase() === prefix + "trigger") {
+        const options = {
+            size: 256,
+          
+            frames: 16
+        }
+
+        message.channel.send("Yapıyorum dostum sakin ol ve bekle.... ⏲").then(m => m.delete(1000));
+
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        const args = message.content.split(' ').slice(1);
+        let member = message.mentions.users.first()
+        if (args[0] === undefined) member = message.author;
+        let avatarurl = member.avatarURL;
+        if (['jpg', 'jpeg', 'gif', 'png', 'webp'].some(x => args.join(' ').includes(x))) {
+            avatarurl = args.join(' ').replace(/gif|webp/g, 'png');
+        }
+        const base = new Jimp(options.size, options.size);
+        const avatar = await Jimp.read(avatarurl);
+        const text = await Jimp.read('https://cdn.glitch.com/a7d3b6b8-9b7a-4aab-9ee4-1db0c07ef1eb%2Ftriggered.png?1526842782410');
+        const tint = await Jimp.read('https://cdn.glitch.com/5fed2789-b430-43c5-bf1b-a8dd32d46b97%2Fred.png?1527082445373');
+        avatar.resize(320, 320);
+        tint.scaleToFit(base.bitmap.width, base.bitmap.height);
+        tint.opacity(0.2);
+        text.scaleToFit(280, 60);
+        const frames = [];
+        const buffers = [];
+        const encoder = new GIFEncoder(options.size, options.size);
+        const stream = encoder.createReadStream();
+        let temp;
+
+        stream.on('data', async buffer => await buffers.push(buffer));
+        stream.on('end', async () => {
+            return await message.channel.send({
+                files: [{
+                    name: 'notechtriggered.gif',
+                    attachment: Buffer.concat(buffers)
+                }]
+            });
+        });
+        for (let i = 0; i < options.frames; i++) {
+            temp = base.clone();
+            if (i === 0) {
+                temp.composite(avatar, -16, -16);
+            } else {
+                temp.composite(avatar, -32 + getRandomInt(-16, 16), -32 + getRandomInt(-16, 16));
+            }
+            temp.composite(tint, 0, 0);
+            if (i === 0) temp.composite(text, -10, 200);
+            else temp.composite(text, -12 + getRandomInt(-8, 8), 200 + getRandomInt(-0, 12));
+            frames.push(temp.bitmap.data);
+        }
+        encoder.start();
+        encoder.setRepeat(0);
+        encoder.setDelay(20);
+        for (const frame of frames) {
+            encoder.addFrame(frame);
+        }
+        encoder.finish();
+    }
+})
+
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("KICK_MEMBERS")) permlvl = 1;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
+  
